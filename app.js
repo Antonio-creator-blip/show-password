0ce1a93502fdb1b2d708d827cadb602e77824154
Antonio-creator-blip <acrisci892@gmail.com>
2020-11-23 21:45:55 +0100

    showEye

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+let contatore = 0;
 function showPassword() {
+    contatore ++
     var x = document.getElementById("user");
     if (x.type === "password") {
       x.type = "text";
+      document.getElementById('eye').style.visibility = 'visible'; 
     } else {
       x.type = "password";
+      document.getElementById('eye').style.visibility = 'hidden'; 
     }
   }
 
+
+  
