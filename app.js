895e2e9f3ba909a74e97ec8350afc40a3d3babb0
Antonio-creator-blip <creatorcrisci@gmail.com>
2020-11-24 08:34:14 +0100

    OK

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-let contatore = 0;
-
 function showPassword() {
     contatore++
-    var x = document.getElementById("user");
+    let x = document.getElementById("user");
     if (x.type === "password") {
         x.type = "text";
         document.getElementById('eye').style.display = 'block';
